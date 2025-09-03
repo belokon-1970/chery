71d2712cd1568529af0c2cdd4fc6a5b3df8542a5
buka- <buka-roc-start@yandex.ru>
2025-09-03 13:26:17 +0300

    Просто обновление

--- chery/main/urls.py
+++ chery/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views, admin
 
 urlpatterns = [
-    path('',views.index,),
-    path('about111', views.about,),
+    path('',views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('modeli', views.modeli, name='modeli'),
 ]
