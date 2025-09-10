aa0f8f8f5afe1da66b82b4b500c5e4f7bf62c32e
buka- <buka-roc-start@yandex.ru>
2025-09-10 21:05:40 +0300

    vv

--- chery/main/urls.py
+++ chery/main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.index, name='home'),
     path('about', views.about, name='about'),
     path('kompaniya', views.kompaniya, name='kompaniya'),
+    path('modeli', views.modeli, name='modeli'),
 ]
