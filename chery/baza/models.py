269b094b325c8afc08a0c244fa2c1dd4ff8b3cfa
Александр Сон <aleksandrson@Aleksandrs-MacBook-Air-6.local>
2025-09-10 13:34:12 +0300

    Added Picture on baza page

--- chery/baza/models.py
+++ chery/baza/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Artiles(models.Model):
     title = models.CharField('Название', max_length=50)
     annons = models.CharField('Анонс', max_length=250)
+    image = models.ImageField(verbose_name="Картинка", null=True, blank=True)
     full_text = models.TextField('Статья')
     date = models.DateTimeField('Дата публикации')
 
